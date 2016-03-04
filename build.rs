c04613cad2e531723ebf4966af5ed6f18dba93b8
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2016-03-04 16:38:14 +0100

    Remove unused import; add FIXME

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::process::Command;
 
 fn main() {
@@ -7,6 +6,7 @@ fn main() {
         .status().unwrap();
 
     println!("cargo:rustc-link-search=native=highwayhash/bazel-out/local_linux-opt/bin");
+    //^ FIXME: This only works on Linux.
     println!("cargo:rustc-link-lib=static=highway_tree_hash");
     println!("cargo:rustc-link-lib=static=sip_tree_hash");
     println!("cargo:rustc-link-lib=static=sip_hash");
