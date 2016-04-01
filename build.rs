9168fcd106b9a07df842dca21d3540ca74e5964d
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2016-04-01 15:13:40 +0200

    More helpful error if bazel fails

--- build.rs
+++ build.rs
@@ -3,7 +3,9 @@ use std::process::Command;
 fn main() {
     std::env::set_current_dir("highwayhash").unwrap();
     Command::new("bazel").args(&["build", ":all", "-c", "opt", "--copt=-mavx2", "--copt=-fPIC"])
-        .status().unwrap();
+        .status()
+        .expect("Failed to run bazel. \
+                 Please make sure it is installed and that your CPU supports AVX2.");
 
     println!("cargo:rustc-link-search=native=highwayhash/bazel-out/local_linux-opt/bin");
     //^ FIXME: This only works on Linux.
